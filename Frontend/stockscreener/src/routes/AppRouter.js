1837f7fcbbc2cd70882e8453aa88845719226141
abhayg44 <141438982+abhayg44@users.noreply.github.com>
2025-08-30 15:52:32 +0530

    small changes

--- Frontend/stockscreener/src/routes/AppRouter.js
+++ Frontend/stockscreener/src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "../Pages/HomePage";
+import HomePage from "../Pages/Homepage";
 import RegisterPage from "../Pages/RegisterPage";
 import Watchlist from "../Pages/Watchlist";
 
