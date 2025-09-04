8c7858c9a452ae5796119d922062bac23623b842
abhayg44 <141438982+abhayg44@users.noreply.github.com>
2025-09-04 20:30:45 +0530

    Stock screening logic implemented

--- Backend/middleware/RateLimiter.js
+++ Backend/middleware/RateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require("express-rate-limit");
 
 const RegisterLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 15 * 60 * 1000,
   max: 5, // max 5 requests per IP per window
   message: "Too many accounts created from this IP, please try again later",
 });
